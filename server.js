c6f4e02ec796a8b12ba7b6dcf4c72bded2438e15
Andeser-rgb <andeser@protonmail.com>
2021-07-24 10:53:54 +0200

    Debug test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -16,9 +17,18 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let urlArray = [];
+
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  console.log(req.body);
+  let url = req.body.url
+  console.log(url);
+  console.log(dns.lookup(url));
+  console.log(dns.lookup('ciao'));
+});
+
+app.get('/api/shorturl/:number', (req, res) => {
+
 });
 
 app.listen(port, function() {
