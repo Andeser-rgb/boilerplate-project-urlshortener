c0b95fd43861cb5eaffebc077faa29207c39ae33
Andeser-rgb <andeser@protonmail.com>
2021-07-24 10:40:52 +0200

    Trying to implement post request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = requite('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,14 +10,15 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);
 });
 
 app.listen(port, function() {
