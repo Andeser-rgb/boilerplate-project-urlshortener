d493a82bfe09c5f72d1c59b84c620696904fb65d
Andeser-rgb <andeser@protonmail.com>
2021-07-25 21:23:26 +0200

    Fixed some problems with the database

--- server.js
+++ server.js
@@ -22,50 +22,50 @@ app.get('/', function(req, res) {
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const shortUrl = new mongoose.Schema({
+const urlSchema = new mongoose.Schema({
   original_url: String,
   short_url: Number
 });
 
-let ShortModel = new mongoose.model("ShortModel" ,shortUrl);
+let UrlModel = new mongoose.model("UrlModel" , urlSchema);
 
 let lastIndex = 1;
 
+UrlModel.find({}, (err, data) => {
+  lastIndex = data.length + 1;
+});
+
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  let url = req.body.url;
-  url = url.replace(/https?:\/\//, "").replace(/\/$/, "").split('/')[0];
+  let url = req.body.url
+      .replace(/https?:\/\//, "")
+      .replace(/\/$/, "")
+      .split('/')[0];
+
   dns.lookup(url, err => {
     if(err) res.send({error: "Invalid url"});
-    else {
-      ShortModel.find({original_url: req.body.url}, (err, data) => {
-        console.log(err);
-        console.log(data);
-        if(true){
-          
-          //res.json({original_url: req.body.url, short_url: lastIndex});
-          res.json(data);
-          const short = new ShortModel({original_url: req.body.url, short_url: lastIndex++});
-          short.save((err, data) => {
-            console.log(err);
-          });
-          
+    else UrlModel.find({original_url: req.body.url}, (err, data) => {
+        if(data.length === 0){
+          const urlObject = {original_url: req.body.url, short_url: lastIndex++};
+          res.json(urlObject);
+          const urlDoc = new UrlModel(urlObject);
+          urlDoc.save((err, data) => {console.log(err);});
         }
-        else res.json(data);
-        
-      })
-      
-    }
+        else res.json({
+          original_url: data[0].original_url,
+          short_url: data[0].short_url
+        });
+    });
   });
-
 });
-/*
 app.get('/api/shorturl/:number', (req, res) => {
   let number = req.params.number;
-  console.log(urlArray[number - 1]);
-  if(number < lastIndex) res.redirect(urlArray[number - 1]);
+  UrlModel.find({short_url: number}, (err, data) => {
+    if(data.length <= 0) console.log(err);
+    else res.redirect(data[0].original_url);
+  });
 });
-*/
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
