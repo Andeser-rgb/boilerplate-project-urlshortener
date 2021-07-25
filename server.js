f26cdbf3240e2420fdbac87e1a3d9b5b0f1804b1
Imme <68339125+Andeser-rgb@users.noreply.github.com>
2021-07-25 16:30:07 +0000

    Database

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const dns = require('dns');
+const mongoose = require('mongoose');
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,21 +20,50 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-let urlArray = [];
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const shortUrl = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+let ShortModel = new mongoose.model("ShortModel" ,shortUrl);
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
   let url = req.body.url;
-  console.log(url);
+  url = url.replace(/https?:\/\//, "").replace(/\/$/, "").split('/')[0];
   dns.lookup(url, err => {
-    if(err) res.send({error: "Invalid url"})
+    if(err) res.send({error: "Invalid url"});
+    else {
+      ShortModel.find({original_url: req.body.url}, (err, data) => {
+        console.log(err);
+        console.log(data);
+        if(true){
+          
+          //res.json({original_url: req.body.url, short_url: lastIndex});
+          res.json(data);
+          const short = new ShortModel({original_url: req.body.url, short_url: lastIndex++});
+          short.save((err, data) => {
+            console.log(err);
+          });
+          
+        }
+        else res.json(data);
+        
+      })
+      
+    }
   });
-});
 
+});
+/*
 app.get('/api/shorturl/:number', (req, res) => {
-
+  let number = req.params.number;
+  console.log(urlArray[number - 1]);
+  if(number < lastIndex) res.redirect(urlArray[number - 1]);
 });
-
+*/
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
